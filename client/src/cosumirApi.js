e5b8681ceb81a483e787aa94bd64b3ca5371a894
QuintansC <gustavosantos.2095@gmail.com>
2021-09-25 02:55:35 -0300

    refactor: remove client

--- client/src/cosumirApi.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-const api = axios.create({
-    baseURL: 'http://localhost:5000/', //Endereço da api, ou seja Localhost:Porta da Api ou Endereço web:Porta
-});
-
-export default api;
