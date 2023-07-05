6d417489586ca82d4efffb93ff6b898e6ceec2f6
QuintansC <gustavosantos.2095@gmail.com>
2023-07-04 21:44:56 -0300

    fix(variaveis): corrigindo vazios que dao erro no mongoDB

--- src/services/database.service.ts
+++ src/services/database.service.ts
@@ -3,8 +3,8 @@ import * as dotenv from 'dotenv'
 
 dotenv.config();
 
-const URL_DB = process.env.DB_LOGIN ? `mongodb+srv://${process.env.DB_LOGIN}/?retryWrites=true&w=majority`: '';
+const URL_DB = process.env.DB_LOGIN ? `mongodb+srv://${process.env.DB_LOGIN}/?retryWrites=true&w=majority` : 'mongodb://a:1/';
 export const client = new MongoClient(URL_DB);
-export const userCollection: Collection = client.db(process.env.DB_NAME).collection(process.env.LOGIN_COLLECTION ? process.env.LOGIN_COLLECTION:'');
+export const userCollection: Collection = client.db(process.env.DB_NAME).collection(process.env.LOGIN_COLLECTION ? process.env.LOGIN_COLLECTION: 'TESTE');
 export const workspacesCollection: Collection = client.db(process.env.DB_NAME).collection('workspaces');
 export const categoriesCollection: Collection = client.db(process.env.DB_NAME).collection('categories');
