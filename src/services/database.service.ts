94f2f21a0ea5e04ac46cd067d8e081242d088ba5
QuintansC <gustavosantos.2095@gmail.com>
2023-07-04 21:26:14 -0300

    fix(secrets): ajustando corretamente os segredos

--- src/services/database.service.ts
+++ src/services/database.service.ts
@@ -2,7 +2,9 @@ import { MongoClient, Collection } from "mongodb";
 import * as dotenv from 'dotenv'
 
 dotenv.config();
-export const client = new MongoClient(process.env.CUSTOMCONNSTR_DB_CONN_STRING ? process.env.CUSTOMCONNSTR_DB_CONN_STRING: '');
-export const userCollection: Collection = client.db(process.env.CUSTOMCONNSTR_DB_NAME).collection(process.env.CUSTOMCONNSTR_LOGIN_COLLECTION_NAME ? process.env.CUSTOMCONNSTR_LOGIN_COLLECTION_NAME:'');
-export const workspacesCollection: Collection = client.db(process.env.CUSTOMCONNSTR_DB_NAME).collection('workspaces');
-export const categoriesCollection: Collection = client.db(process.env.CUSTOMCONNSTR_DB_NAME).collection('categories');
+
+const URL_DB = process.env.DB_LOGIN ? `mongodb+srv://${process.env.DB_LOGIN}/?retryWrites=true&w=majority`: '';
+export const client = new MongoClient(URL_DB);
+export const userCollection: Collection = client.db(process.env.DB_NAME).collection(process.env.LOGIN_COLLECTION ? process.env.LOGIN_COLLECTION:'');
+export const workspacesCollection: Collection = client.db(process.env.DB_NAME).collection('workspaces');
+export const categoriesCollection: Collection = client.db(process.env.DB_NAME).collection('categories');
